fb70e2cb27ff100d0afa18a6a0dff006b0ad60af
nadav <nadav.abramovich@gmail.com>
2024-12-25 11:40:46 +0200

    Basic structure of the project. I wouldv'e added tests at this stage, but because I'm about to use a new library I know nothing of, I believe I won't be able to make good decisions about which tests to make at this stage.
    I initially thought about making clockHandler a class with only statis methods, but it is probably over-engineering it as no instances of it will ever be made.

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+This module will allow me to locally call functions as I develop
+the program. It won't be a part of the program once it is done.
+Perhaps I won't be very tidy in this module as it isnt part of the final program.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
